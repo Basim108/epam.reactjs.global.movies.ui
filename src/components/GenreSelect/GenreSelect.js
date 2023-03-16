3d3713c18c101a3dfa12212e93a200d073465c40
Basim Al-Jawahery <basimdev@gmail.com>
2023-03-16 18:48:40 +0400

    refact: simplified GenreSelect component

--- src/components/GenreSelect/GenreSelect.js
+++ src/components/GenreSelect/GenreSelect.js
@@ -1,52 +1,43 @@
-import {useRef, useState} from 'react';
-import PropTypes          from 'prop-types';
-import styles             from './GenreSelect.module.css';
+import {useState} from 'react';
+import PropTypes  from 'prop-types';
+import styles     from './GenreSelect.module.css';
 
 
 const GenreSelect = (props) => {
-    const [activeGenre, setActiveGenre] = useState(props.activeGenre)
+    const buildClasses = (title, active) => styles.genre + (title === active
+                                                            ? ' ' + styles.selectedGenre
+                                                            : '')
 
-    const genresRef   = useRef(null);
-    const getMap      = () => genresRef.current || (genresRef.current = new Map());
-    const selectGenre = genre => {
+    const [activeGenre, setActiveGenre] = useState(props.activeGenre)
+    const [genreList, setGenreList]     = useState(props.genreList
+                                                        .map(title => ({
+                                                            title, className: buildClasses(title, activeGenre)
+                                                        })))
+    const selectGenre                   = genre => {
         if (genre === activeGenre) {
             return
         }
-        const map               = getMap()
-        const currentSelectedEl = map.get(activeGenre)
-        const nextSelectedEl    = map.get(genre)
-        currentSelectedEl.classList.remove(styles.selectedGenre)
-        nextSelectedEl.classList.add(styles.selectedGenre)
         setActiveGenre(genre)
-        if (props.onSelect) {
-            props.onSelect(genre)
-        }
+        setGenreList(genreList.map(info => {
+            info.className = buildClasses(info.title, genre)
+            return info
+        }))
+        props.onSelect(genre)
     }
 
-    return (
-        <div className={styles.GenreSelect + ' row mt-2'}>
-            <div className="col">
-                {props.genreList.map(genre => {
-                    const genreClassName = styles.genre + (genre === activeGenre
-                                                           ? ' ' + styles.selectedGenre
-                                                           : '')
-                    const ref            = el => el
-                                                 ? getMap().set(genre, el)
-                                                 : getMap().delete(genre)
-                    return (<span className={genreClassName}
-                                  ref={ref}
-                                  onClick={() => selectGenre(genre)}
-                                  key={genre}>{genre}</span>)
-                })}
-            </div>
+    return (<div className={styles.GenreSelect + ' row mt-2'}>
+        <div className="col">
+            {genreList.map(info =>
+                               <span key={info.title}
+                                      className={info.className}
+                                      onClick={() => selectGenre(info.title)}
+                               > {info.title} </span>)}
         </div>
-    )
+    </div>)
 }
 
 GenreSelect.propTypes = {
-    genreList  : PropTypes.arrayOf(PropTypes.string).isRequired,
-    activeGenre: PropTypes.string.isRequired,
-    onSelect   : PropTypes.func.isRequired
+    genreList: PropTypes.arrayOf(PropTypes.string).isRequired, activeGenre: PropTypes.string.isRequired, onSelect: PropTypes.func.isRequired
 };
 
 export default GenreSelect;
