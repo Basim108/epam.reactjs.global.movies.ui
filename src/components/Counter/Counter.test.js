c7f2f1b9fda9b9508b6c75d9e679fdc0275b9c89
Basim Al-Jawahery <basimdev@gmail.com>
2023-03-17 20:54:48 +0400

    test: cover Counter component

---
+++ src/components/Counter/Counter.test.js
@@ -0,0 +1,38 @@
+﻿import {render, screen} from '@testing-library/react';
+import Counter          from "./Counter";
+import {act}            from "react-dom/test-utils";
+import '@testing-library/jest-dom/extend-expect';
+
+describe('<Counter />', () => {
+    test('it should mount', () => {
+        render(<Counter initialValue={1}/>);
+        const counter = screen.getByTestId('Counter');
+        expect(counter).toBeInTheDocument();
+    })
+
+    test('given initial value should render it', () => {
+        render(<Counter initialValue={5}/>);
+        const counter = screen.getByTestId('Counter');
+        expect(counter).toHaveTextContent('5');
+    })
+
+    test('when click on increase btn should increment value', () => {
+        render(<Counter initialValue={5}/>);
+        const increaseBtn = screen.getByRole('button', {name: /\+/i})
+        act(() => {
+            increaseBtn.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+        });
+        const counter = screen.getByTestId('Counter');
+        expect(counter).toHaveTextContent('6');
+    })
+
+    test('when click on decrease btn should decrement value', () => {
+        render(<Counter initialValue={5}/>);
+        const decreaseBtn = screen.getByRole('button', {name: /-/i})
+        act(() => {
+            decreaseBtn.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+        });
+        const counter = screen.getByTestId('Counter');
+        expect(counter).toHaveTextContent('4');
+    })
+})
