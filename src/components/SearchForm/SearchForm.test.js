64fc8daf0e083f74fd62fa42ab16afc908e3e938
Basim Al-Jawahery <basimdev@gmail.com>
2023-03-17 23:06:25 +0400

    test: cover SearchForm component

--- src/components/SearchForm/SearchForm.test.js
+++ src/components/SearchForm/SearchForm.test.js
@@ -1,12 +1,48 @@
-import React            from 'react';
-import {render, screen} from '@testing-library/react';
+import React                       from 'react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import SearchForm       from './SearchForm';
+import {act}            from "react-dom/test-utils";
 
 describe('<SearchForm />', () => {
-    test('it should mount', () => {
-        render(<SearchForm onSearch={() => null}/>);
-        // const searchForm = screen.getByTestId('SearchForm');
-        // expect(searchForm).toBeInTheDocument();
+    test('given query in props should have query in input value', () => {
+        render(<SearchForm query="hello" onSearch={() => null}/>);
+        const searchForm = screen.getByRole('textbox', {value: /hello/i});
+        expect(searchForm).toBeInTheDocument();
+    });
+
+    test('given value in input when click on search btn should call onSearch callback', () => {
+        const onSearch = jest.fn()
+        render(<SearchForm onSearch={onSearch}/>);
+        
+        const searchInput = screen.getByRole('textbox');
+        expect(searchInput).toBeInTheDocument();
+        fireEvent.change(searchInput, {target: {value: 'test-value'}})
+        
+        const searchBtn = screen.getByRole('button', { name: /Search/i});
+        expect(searchBtn).toBeInTheDocument();
+        
+        act(() => {
+            searchBtn.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+        });
+
+        expect(onSearch).toHaveBeenCalledTimes(1)
+        expect(onSearch).toHaveBeenCalledWith('test-value')
+    });
+
+    test('given value in input when press Enter should call onSearch callback', () => {
+        const onSearch = jest.fn()
+        render(<SearchForm onSearch={onSearch}/>);
+        
+        const searchInput = screen.getByRole('textbox');
+        expect(searchInput).toBeInTheDocument();
+        fireEvent.change(searchInput, {target: {value: 'test-value'}})
+        
+        act(() => {
+            fireEvent.keyDown(searchInput, {key: 'Enter', code: 'Enter', charCode: 13})
+        });
+
+        expect(onSearch).toHaveBeenCalledTimes(1)
+        expect(onSearch).toHaveBeenCalledWith('test-value')
     });
 });
