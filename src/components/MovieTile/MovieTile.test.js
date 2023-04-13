d8b340e99d1838f5689fced3baeee39e4d95bfde
Basim Al-Jawahery <basimdev@gmail.com>
2023-04-13 10:36:19 +0400

    test(MovieTile): optimized with beforeEach

--- src/components/MovieTile/MovieTile.test.js
+++ src/components/MovieTile/MovieTile.test.js
@@ -5,42 +5,41 @@ import MovieTile        from './MovieTile';
 import userEvent        from "@testing-library/user-event";
 
 describe('<MovieTile />', () => {
-    const movie = {
-        genres     : ['Action', 'Adventure', 'Drama'],
-        imageUrl   : '/assets/img/some.png',
-        title      : 'Avengers',
-        releaseYear: 2004
-    }
+    let movie;
+
+    beforeEach(() => {
+        movie = {
+            genres     : ['Action', 'Adventure', 'Drama'],
+            imageUrl   : '/assets/img/some.png',
+            title      : 'Avengers',
+            releaseYear: 2004
+        }
+        render(<MovieTile {...movie} />);
+    })
 
     test('should mount it', () => {
-        render(<MovieTile {...movie} />);
         expect(screen.getByTestId('MovieTile')).toBeInTheDocument();
     });
 
     test('should render releaseYear', () => {
-        render(<MovieTile {...movie} />);
         expect(screen.getByText(movie.releaseYear)).toBeInTheDocument();
     });
 
     test('should render title', () => {
-        render(<MovieTile {...movie} />);
         expect(screen.getByText(movie.title)).toBeInTheDocument();
     });
 
     test('should join genres with comma', () => {
-        render(<MovieTile {...movie} />);
         expect(screen.getByText(movie.genres.join(', '))).toBeInTheDocument();
     });
 
     test('should render movie image', () => {
-        render(<MovieTile {...movie} />);
         const image = screen.getByAltText(movie.title)
         expect(image).toBeInTheDocument()
         expect(image.src).toEqual(`http://localhost${movie.imageUrl}`)
     });
 
     test('should render movie context menu', async() => {
-        render(<MovieTile {...movie} />);
         const button = screen.getByTestId('MovieContextMenuButton')
         expect(button).toBeInTheDocument()
         await userEvent.click(button)
